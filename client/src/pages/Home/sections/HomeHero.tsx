431a623f65c0cabfe5d88bc3769d745511d39f6b
Zinazero <jacobhexamer@gmail.com>
2025-09-25 16:08:52 -0400

    feat: added hero video as per client request

--- client/src/pages/Home/sections/HomeHero.tsx
+++ client/src/pages/Home/sections/HomeHero.tsx
@@ -1,29 +1,19 @@
-import { Link } from 'react-router-dom';
-import videoThumbnail from '@/assets/images/generic/video-thumbnail.jpg';
-import { Image } from '../../../components/ui/Image';
+import { useNavigate } from 'react-router-dom';
+import { HeroVideo } from '../../../components/ui/HeroVideo';
+import heroMp4 from '@/assets/videos/hero-video.mp4';
 
 export const Hero = () => {
+	const navigate = useNavigate();
+
 	return (
-		<section id='home-hero'>
-			<div className='flex flex-col xl:flex-row items-center justify-evenly space-y-10 xl:space-y-0 xl:space-x-16 p-20'>
-				<Image
-					src={videoThumbnail}
-					alt='Video Thumbnail'
-					className='max-w-240 rounded-lg shadow-md'
+		<section id='home-hero' className='h-[50vh]'>
+				<HeroVideo
+					videoSrc={heroMp4}
+					title='Ontario’s Premier Park and Play Equipment Provider'
+					subtitle='Building Worlds of Fun'
+					ctaText='See Our Products'
+					onCtaClick={() => navigate('/products')}
 				/>
-				<div className='flex flex-col max-w-240 space-y-8'>
-					<h1 className='text-5xl font-bold text-brand-orange'>
-						Ontario’s Premier Park and Play Equipment Provider
-					</h1>
-					<h4 className='text-2xl'>Building Worlds of Fun</h4>
-					<Link
-						to='products'
-						className='rounded-lg p-2 bg-brand-blue text-light font-semibold w-60 text-center hover:bg-brand-orange transition'
-					>
-						See Our Products
-					</Link>
-				</div>
-			</div>
 		</section>
 	);
 };
