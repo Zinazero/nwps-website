3e24cef47d7c73f9be5ed02111f68863224feb73
Zinazero <jacobhexamer@gmail.com>
2025-09-25 18:44:19 -0400

    fix: add h-full and w-full to Loading for proper centering

--- client/src/components/ui/Loading.tsx
+++ client/src/components/ui/Loading.tsx
@@ -3,7 +3,7 @@ import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 export const Loading = () => {
 	return (
-		<div className='flex items-center justify-center flex-1 text-transparent-grey'>
+		<div className='flex  w-full h-full items-center justify-center flex-1 text-transparent-grey'>
 			<FontAwesomeIcon icon={faSpinner} spin size='2x' />
 		</div>
 	);
