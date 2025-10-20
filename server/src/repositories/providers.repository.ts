7bd1f111a48a313be9f28cd1d93e83fac090acf0
Zinazero <jacobhexamer@gmail.com>
2025-10-20 18:22:54 -0400

    fix: add fetching of slug from database

--- server/src/repositories/providers.repository.ts
+++ server/src/repositories/providers.repository.ts
@@ -11,17 +11,17 @@ interface Provider {
 
 export const getAllProviders = async (): Promise<Provider[]> => {
 	const res: QueryResult<Provider> = await pool.query(
-		'SELECT title, blurb, description, external_link AS "externalLink" FROM providers ORDER BY title'
+		'SELECT title, blurb, description, external_link AS "externalLink", slug FROM providers ORDER BY title'
 	);
 	return res.rows;
 };
 
 export const getProviderBySlug = async (slug: string): Promise<Provider> => {
-    const res: QueryResult<Provider> = await pool.query(
-        'SELECT title, blurb, description, external_link AS "externalLink" FROM providers WHERE slug = $1',
-        [slug]  
-    );
-    return res.rows[0];
+	const res: QueryResult<Provider> = await pool.query(
+		'SELECT title, blurb, description, external_link AS "externalLink" FROM providers WHERE slug = $1',
+		[slug]
+	);
+	return res.rows[0];
 };
 
 export const postProvider = async (provider: Provider) => {
