00b6f5a976967748af4a960ac86e15c2575c2412
Zinazero <jacobhexamer@gmail.com>
2025-11-04 22:35:53 -0500

    feat: don't render recent projects if none are fetched

--- client/src/pages/Home/sections/RecentProjects.tsx
+++ client/src/pages/Home/sections/RecentProjects.tsx
@@ -29,38 +29,40 @@ export const RecentProjects = () => {
   return (
     <section>
       {/* Recent Projects */}
-      <div className={cn('flex flex-col max-w-400 mx-auto px-4 py-14 space-y-10', 'md:px-20')}>
-        <Link to="/portfolio">
-          <div className={cn('flex flex-col items-center w-full group', 'md:flex-row')}>
-            <h2
-              className={cn(
-                'text-5xl font-bold text-nowrap mr-0 transition',
-                'md:mr-6 md:group-hover:translate-x-4',
-              )}
-            >
-              Recent Projects
-            </h2>
-            <hr
-              className={cn(
-                'w-full text-brand-orange transition mt-4',
-                'md:group-hover:translate-x-4 md:mt-0',
-              )}
-            />
-          </div>
-        </Link>
-        {loading ? (
-          <Loading />
-        ) : (
-          <div className="grid grid-cols-1 xl:grid-cols-3 gap-10 text-center">
-            {recentProjects.map((park) => (
-              <div key={park.id}>
-                <ParkCard park={park} className="mb-24" />
-                <p className="text-lg/loose">{park.blurb || park.description}</p>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+      {(loading || recentProjects.length > 0) && (
+        <div className={cn('flex flex-col max-w-400 mx-auto px-4 py-14 space-y-10', 'md:px-20')}>
+          <Link to="/portfolio">
+            <div className={cn('flex flex-col items-center w-full group', 'md:flex-row')}>
+              <h2
+                className={cn(
+                  'text-5xl font-bold text-nowrap mr-0 transition',
+                  'md:mr-6 md:group-hover:translate-x-4',
+                )}
+              >
+                Recent Projects
+              </h2>
+              <hr
+                className={cn(
+                  'w-full text-brand-orange transition mt-4',
+                  'md:group-hover:translate-x-4 md:mt-0',
+                )}
+              />
+            </div>
+          </Link>
+          {loading ? (
+            <Loading />
+          ) : (
+            <div className="grid grid-cols-1 xl:grid-cols-3 gap-10 text-center">
+              {recentProjects.map((park) => (
+                <div key={park.id}>
+                  <ParkCard park={park} className="mb-24" />
+                  <p className="text-lg/loose">{park.blurb || park.description}</p>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
 
       {/* Call To Action Banner */}
       <CallToAction />
