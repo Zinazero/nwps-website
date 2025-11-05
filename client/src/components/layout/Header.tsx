5a0a90a245940d80bc5a6cfb691b7361b82c37d0
Zinazero <jacobhexamer@gmail.com>
2025-11-05 17:00:49 -0500

    feat: logo clikc scrolls to top of page if already on home

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import nwpsLogo from '@/assets/logos/nwps-logo.svg';
 import { useProducts } from '../../contexts/ProductsContext';
 import { cn } from '../../utils/cn';
@@ -10,9 +10,11 @@ import { MobileNavbar } from '../ui/MobileNavbar';
 import { Navbar } from '../ui/Navbar';
 import type { NavbarProps } from '../ui/types';
 import type { LinkType } from './types';
+import { scrollUp } from '../ui/ScrollToTop';
 
 export const Header = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const { productsLinks, loading } = useProducts();
   const isMobile = useIsMobile(1024);
 
@@ -37,9 +39,9 @@ export const Header = () => {
     <header className={cn('fixed top-0 z-50 w-full shadow-md bg-white py-4')}>
       <div className="flex flex-col w-full">
         <div className={cn('px-4 flex items-center justify-between')}>
-          <Link to="/">
+          <button type="button" onClick={() => (location.pathname === '/' ? scrollUp() : navigate('/'))}>
             <Image src={nwpsLogo} alt="NWPS Logo" className={cn('min-w-55 cursor-pointer')} priority />
-          </Link>
+          </button>
 
           {isMobile ? (
             <MenuButton onClick={() => setMobileNavOpen(!mobileNavOpen)} />
