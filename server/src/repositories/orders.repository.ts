bedc80c0fde398283d54e03d0a34df27e1614db2
Zinazero <jacobhexamer@gmail.com>
2025-11-05 17:19:29 -0500

    feat: order number is regenerated if the insert query fails due to a unique constraint

--- server/src/repositories/orders.repository.ts
+++ server/src/repositories/orders.repository.ts
@@ -1,6 +1,6 @@
 import type { QueryResult } from 'pg';
 import pool from '../db';
-import { OrderInfo, OrderItem } from '../types';
+import { DbError, OrderInfo, OrderItem } from '../types';
 import { postOrderItems } from './orderItems.repository';
 
 export const postOrder = async (orderInfo: OrderInfo, cart: OrderItem[]) => {
@@ -19,47 +19,51 @@ export const postOrder = async (orderInfo: OrderInfo, cart: OrderItem[]) => {
   } = orderInfo;
 
   const client = await pool.connect();
+  const year = new Date().getFullYear();
 
+  let orderNumber: string;
+  let inserted = false;
   try {
-    // Generate Order Number
-    const prefix = 'NWPS';
-    const year = new Date().getFullYear();
-    const random = Math.floor(10000 + Math.random() * 90000);
-    const orderNumber = `${prefix}-${year}-${random}`;
+    while (!inserted) {
+      const random = Math.floor(10000 + Math.random() * 90000);
+      orderNumber = `NWPS-${year}-${random}`;
 
-    await client.query('BEGIN');
+      try {
+        await client.query('BEGIN');
 
-    // Insert Order Info into orders table
-    const res: QueryResult<{ id: number }> = await client.query(
-      `INSERT INTO orders 
+        const res: QueryResult<{ id: number }> = await client.query(
+          `INSERT INTO orders 
             (first_name, last_name, company, phone, email, address_1, address_2, city, province, postal_code, message, order_number)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
-            RETURNING id`,
-      [
-        firstName,
-        lastName,
-        company,
-        phone,
-        email,
-        address1,
-        address2,
-        city,
-        province,
-        postalCode,
-        message,
-        orderNumber,
-      ],
-    );
-    const orderId = res.rows[0].id;
+           VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)
+           RETURNING id`,
+          [
+            firstName,
+            lastName,
+            company,
+            phone,
+            email,
+            address1,
+            address2,
+            city,
+            province,
+            postalCode,
+            message,
+            orderNumber,
+          ],
+        );
 
-    // Insert Order Items into order_items table
-    await postOrderItems(orderId, cart, client);
+        const orderId = res.rows[0].id;
+        await postOrderItems(orderId, cart, client);
 
-    await client.query('COMMIT');
-    return orderNumber;
-  } catch (err: unknown) {
-    await client.query('ROLLBACK');
-    throw err;
+        await client.query('COMMIT');
+        inserted = true;
+        return orderNumber;
+      } catch (err: unknown) {
+        const error = err as DbError;
+        await client.query('ROLLBACK');
+        if (error.code !== '23505') throw err;
+      }
+    }
   } finally {
     client.release();
   }
