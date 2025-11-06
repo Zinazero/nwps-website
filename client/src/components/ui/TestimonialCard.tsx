1842b161234994daf80df512872adf7e890c60ed
Zinazero <jacobhexamer@gmail.com>
2025-11-05 23:13:22 -0500

    styles: add responsive styles to testimonials

--- client/src/components/ui/TestimonialCard.tsx
+++ client/src/components/ui/TestimonialCard.tsx
@@ -1,4 +1,5 @@
 import type { Testimonial } from '../../pages/types';
+import { cn } from '../../utils/cn';
 import { Image } from './Image';
 import { ImageMask } from './ImageMask';
 
@@ -14,15 +15,27 @@ export const TestimonialCard = ({ testimonial, index }: TestimonialCardProps) =>
   const priority = index < 2;
 
   return (
-    <div className="rounded-xl shadow-xl bg-white p-10">
-      <div className="flex items-center space-x-10 border-b border-brand-orange pb-8 my-10">
-        <div className="flex flex-col w-1/2 space-y-14">
-          <h3 className="text-2xl font-bold text-brand-blue">{testimonial.title}</h3>
+    <div className={cn('rounded-xl shadow-xl bg-white p-4', 'md:p-10')}>
+      <div
+        className={cn(
+          'flex flex-col-reverse items-center gap-4 border-b border-brand-orange pb-8 my-6',
+          'md:flex-row md:gap-10 md:my-10',
+        )}
+      >
+        <div className={cn('flex flex-col gap-6', 'md:w-1/2 md:gap-14')}>
+          <h3 className={cn('text-2xl text-center font-bold text-brand-blue', 'md:text-left')}>
+            {testimonial.title}
+          </h3>
           <p className="text-lg">{testimonial.blurb}</p>
         </div>
         <div className="flex justify-center w-1/2">
           {testimonial.skipMask ? (
-            <Image src={testimonial.imageSrc} alt={`Testimonial ${index}`} priority={priority} />
+            <Image
+              src={testimonial.imageSrc}
+              alt={`Testimonial ${index}`}
+              priority={priority}
+              className="w-40 md:w-full"
+            />
           ) : (
             <ImageMask
               src={testimonial.imageSrc}
--- client/src/pages/Testimonials/Testimonials.tsx
+++ client/src/pages/Testimonials/Testimonials.tsx
@@ -1,12 +1,13 @@
 import { TestimonialCard } from '../../components/ui/TestimonialCard';
 import { UnderlineHeader } from '../../components/ui/UnderlineHeader';
+import { cn } from '../../utils/cn';
 import { TestimonialsArray } from './TestimonialInfo/TestimonialsArray';
 
 export const Testimonials = () => {
   return (
-    <div className="min-h-screen p-16 flex flex-col items-center justify-center">
+    <div className={cn('min-h-screen flex flex-col items-center justify-center py-8 px-6', 'md:py-16')}>
       <UnderlineHeader text="Testimonials" />
-      <div className="flex flex-col max-w-350 p-16 space-y-12">
+      <div className={cn('flex flex-col max-w-screen py-16 px-4 gap-12', 'md:max-w-350 md:p-16')}>
         {TestimonialsArray.map((testimonial, index) => (
           <TestimonialCard key={testimonial.title} testimonial={testimonial} index={index} />
         ))}
