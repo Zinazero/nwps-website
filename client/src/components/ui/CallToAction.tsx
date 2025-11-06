ac57e1df6e25305b104e01e4a933a6f8160ea23a
Zinazero <jacobhexamer@gmail.com>
2025-11-05 21:56:14 -0500

    styles: ad responsive styles to products page

--- client/src/components/ui/CallToAction.tsx
+++ client/src/components/ui/CallToAction.tsx
@@ -8,14 +8,14 @@ export const CallToAction = () => {
     <div
       className={cn(
         'max-w-300 mx-4 rounded-2xl shadow-xl bg-brand-blue-light',
-        'p-12 flex flex-col items-center space-y-6',
-        'xl:mx-auto xl:flex-row xl:space-y-0 md:mx-20',
+        'p-8 flex flex-col items-center gap-6',
+        'lg:mx-auto lg:flex-row lg:gap-0 md:p-12',
       )}
     >
       <Image src={nwpsVerticalLogo} alt="NWPS Logo" className="h-48" />
       <div className="flex-1 flex flex-col items-center gap-6 text-center">
         <p className="text-4xl text-brand-orange">Not sure which product to choose?</p>
-        <div className="flex flex-col xl:flex-row gap-2 text-4xl font-bold">
+        <div className={cn('flex flex-col gap-2 text-4xl font-bold', 'lg:flex-row')}>
           <h3 className="text-brand-blue">Call Us</h3>
           <h3 className="text-brand-blue text-nowrap">{'(519) 304-3437'}</h3>
           <span className="text-brand-blue">or</span>
--- client/src/pages/Products/Categories/ProductsPage.tsx
+++ client/src/pages/Products/Categories/ProductsPage.tsx
@@ -9,6 +9,7 @@ import { Pen } from '../../../components/ui/Pen';
 import { useAuth } from '../../../contexts/AuthContext';
 import { cn } from '../../../utils/cn';
 import type { ProductsCategory } from '../../types';
+import { useIsMobile } from '../../../utils/useIsMobile';
 
 interface ProductsSection {
   id: number;
@@ -27,6 +28,7 @@ export const ProductsPage = () => {
 
   const { category: slug } = useParams<{ category: string }>();
   const navigate = useNavigate();
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -66,24 +68,33 @@ export const ProductsPage = () => {
   };
 
   return (
-    <div className="relative min-h-screen flex flex-col items-center justify-center bg-white space-y-20 pb-26">
+    <div className="relative min-h-screen flex flex-col items-center justify-center bg-white gap-20 pb-26">
       {loading ? (
         <Loading />
       ) : (
         <>
-          <div className="flex flex-col items-center w-full min-h-screen space-y-20 p-6">
+          <div className="flex flex-col items-center w-full min-h-screen gap-20 p-6">
             {/* Hero */}
             {category && (
               <div className="flex w-full items-center justify-center border-b-3 border-dashed border-brand-orange pb-16">
-                <div className="flex items-center max-w-450 space-x-30">
-                  <ImageMask
-                    src={`/images/products/${slug}/${slug}-1.jpg`}
-                    alt={`${category.title} Image 1`}
-                    mask="rock-mask.svg"
-                    priority
-                  />
-                  <div className="flex flex-col space-y-6 w-150">
-                    <h1 className="text-6xl font-bold">{category.title}</h1>
+                <div className={cn('flex flex-col items-center max-w-450 gap-x-30 gap-y-6', 'md:flex-row')}>
+                  {isMobile ? (
+                    <Image
+                      src={`/images/products/${slug}/${slug}-1.jpg`}
+                      alt={`${category.title} Category Photo`}
+                      priority
+                      className="rounded-xl"
+                    />
+                  ) : (
+                    <ImageMask
+                      src={`/images/products/${slug}/${slug}-1.jpg`}
+                      alt={`${category.title} Category Photo`}
+                      mask="rock-mask.svg"
+                      priority
+                    />
+                  )}
+                  <div className={cn('flex flex-col gap-6 text-center', 'md:text-left md:w-150')}>
+                    <h1 className={cn('text-5xl font-bold', 'md:text-6xl')}>{category.title}</h1>
                     <p className="text-xl/relaxed">{category.description}</p>
                   </div>
                 </div>
@@ -96,14 +107,15 @@ export const ProductsPage = () => {
                 <div
                   key={section.title}
                   className={cn(
-                    'flex items-center justify-center max-w-350',
-                    index % 2 !== 0 ? 'flex-row-reverse' : '',
+                    'flex flex-col-reverse items-center justify-center max-w-350 gap-4 text-center',
+                    'md:flex-row md:gap-10 md:text-left',
+                    index % 2 !== 0 ? 'md:flex-row-reverse' : '',
                   )}
                 >
-                  <div className="flex flex-col space-y-4 mx-10 w-1/2">
+                  <div className={cn('flex flex-col gap-4', 'md:w-1/2')}>
                     <h2 className="text-4xl font-semibold text-brand-blue">{section.title}</h2>
                     {section.subheading && <h3 className="text-2xl font-semibold">{section.subheading}</h3>}
-                    <p className="text-lg/relaxed">{section.description}</p>
+                    <p className="text-lg/relaxed text-left!">{section.description}</p>
                   </div>
                   {category && (
                     <Image
