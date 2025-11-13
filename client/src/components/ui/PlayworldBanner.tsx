667c413456988bed636373a5c518bab0285f07f7
Zinazero <jacobhexamer@gmail.com>
2025-11-13 17:49:41 -0500

    style: asdjust responsiveness

--- client/src/components/ui/PlayworldBanner.tsx
+++ client/src/components/ui/PlayworldBanner.tsx
@@ -9,12 +9,13 @@ export const PlayworldBanner = () => {
   return (
     <article id={playworldBannerId} className="bg-brand-blue-light shadow-md w-full">
       <div
-        className={cn('flex flex-col lg:flex-row items-center justify-center space-y-4 lg:space-y-0 p-10')}
+        className={cn('flex flex-col lg:flex-row items-center justify-center gap-10 p-10')}
       >
+        {/* Text */}
         <div
           className={cn(
-            'w-90 flex flex-col items-center text-center space-y-4 justify-between',
-            'lg:w-140 lg:items-start lg:text-left',
+            'w-160 flex flex-col items-center text-center gap-4 justify-between',
+            'lg:items-start lg:text-left',
           )}
         >
           <Image src="/logos/pw-logo.svg" alt="Playworld Logo" className="w-70" priority />
@@ -27,6 +28,8 @@ export const PlayworldBanner = () => {
             <span className="sr-only"> About Playworld</span>
           </Link>
         </div>
+
+        {/* Image */}
         <Image
           className="w-140 rounded-lg shadow-md"
           src="/images/generic/playworld-image.jpg"
