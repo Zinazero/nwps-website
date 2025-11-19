df20ab8386be65ed205b4bc0d42a0fd6a7f93a2f
Zinazero <jacobhexamer@gmail.com>
2025-11-19 14:58:03 -0500

    chore: cleanup use of NODE_ENV

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,5 +6,4 @@ const viteEnv =
 export const CLIENT_BASE = viteEnv.VITE_CLIENT_BASE || 'http://localhost:5173';
 export const SERVER_BASE = viteEnv.VITE_SERVER_BASE || 'http://localhost:5004';
 export const OPTIMIZER_BASE = viteEnv.VITE_IMAGE_OPTIMIZER_BASE || 'http://localhost:4000';
-export const NODE_ENV = viteEnv.VITE_NODE_ENV || 'development';
 export const STORE_ENABLED = viteEnv.VITE_STORE_ENABLED === 'true' || false;
