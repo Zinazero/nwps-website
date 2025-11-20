7da47c43a22cc783468d5e4f7b7623a848cec837
Zinazero <jacobhexamer@gmail.com>
2025-11-20 17:35:24 -0500

    fix: query mistakes

--- server/src/repositories/registrationTokens.repository.ts
+++ server/src/repositories/registrationTokens.repository.ts
@@ -2,7 +2,7 @@ import pool from '../db';
 
 export const createRegistrationToken = async (email: string, token: string, expiresAt: Date) => {
   await pool.query(
-    'INSERT INTO registration_tokens (email, token, expires_at) VALUES ($1, $2) RETURNING id, username',
+    'INSERT INTO registration_tokens (email, token, expires_at) VALUES ($1, $2, $3)',
     [email, token, expiresAt],
   );
 };
